9b8b0f4599a6243abfa669222c600276d7d5e878
John Soros <john@marsnet.org>
2010-10-08 15:21:58 +0200

    add way to generate corpus from inferno ircfs logs

---
+++ makecorp.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd $HOME/src/inferno-os/usr/johnny/irclog/irc.freenode.net
+mkdir /tmp/nickcorpus/
+for nick in francharb alex_a nahuel Christophe_c stephane_n hek eiro matts soul9; do
+  for i in *; do
+    egrep '^(\+|\-) [0-9][0-9]:[0-9][0-9] *'$nick':' "$i" |sed -E 's,^(\+|\-) [0-9][0-9]:[0-9][0-9] *'$nick': ,,g'  >>/tmp/nickcorpus/$nick
+  done
+done
