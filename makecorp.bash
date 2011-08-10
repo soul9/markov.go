6d200362f7a496cea0fc4af6d327ee0c25343f7e
John Soros <johnny@r0x0r.me>
2011-08-10 18:13:47 +0200

    make makecorp smarter

--- makecorp.bash
+++ makecorp.bash
@@ -1,8 +1,47 @@
 #!/bin/bash
-cd $HOME/src/inferno-os/usr/johnny/irclog/irc.freenode.net
-mkdir /tmp/nickcorpus/
-for nick in uriel Kivutar Nahuel kivutar francharb alex_a nahuel Christophe_c stephane_n hek eiro matts soul9; do
-  for i in *; do
-    egrep '^(\+|\-) [0-9][0-9]:[0-9][0-9] *'$nick':' "$i" |sed -E 's,^(\+|\-) [0-9][0-9]:[0-9][0-9] *'$nick': ,,g'  >>/tmp/nickcorpus/$nick
+mkinfernocorp() {
+  usage='mkinfernocorp $nick $channel $basedir $outfile'
+  if [[  -z $1 ||  -z $2 ||  -z $3 ||  -z $4 ]]; then
+      echo $usage
+      return
+  fi
+  cd $3 || ( echo "No such directory: $3" && return )
+  touch $4 || ( echo "Can't create $4" && return )
+  egrep '^(\+|\-) [0-9][0-9]:[0-9][0-9] *'$1':' "${2}.log" |sed -E 's,^(\+|\-) [0-9][0-9]:[0-9][0-9] *'$1': ,,g'  >> $4
+}
+
+mkznccorp() {
+  usage='usage: mkznccorp $nick $channel $basedir $outfile'
+  if [[  -z $1 ||  -z $2 ||  -z $3 ||  -z $4 ]]; then
+    echo $usage
+    return
+  fi
+  cd $3 || ( echo "No such directory: $3" && return )
+  touch $4 || ( echo "Can't create $4" && return )
+  ( for file in "${2}"*; do
+    egrep -v "^\[[0-9:]+\] \*\*\*.*" $file| egrep "^\[[0-9:]+\] <$1>"  |sed -E 's,^\[[0-9:]+\] <[a-zA-Z0-9_-]+> ,,g'
+  done ) >> $4
+}
+
+infernobasedir=$HOME/src/inferno-os/usr/johnny/irclog/irc.freenode.net
+zncbasedir=$HOME/.znc/users/KBme/moddata/log
+
+corpdir=/tmp/corps
+mkdir $corpdir
+
+for nick in eiro mc khatar; do
+  for chan in "#biblibre" "#soul9"; do
+    mkinfernocorp $nick $chan $infernobasedir $corpdir/eirocorp
+    mkznccorp $nick $chan $zncbasedir $corpdir/eirocorp
   done
+done
+
+for nick in soul9 KBme; do
+  mkinfernocorp $nick '#soul9' $infernobasedir $corpdir/soul9corp
+  mkznccorp $nick '#soul9' $zncbasedir $corpdir/soul9corp
+done
+
+for nick in clrh alex_a francharb kivutar; do
+  mkinfernocorp $nick '#soul9' $infernobasedir $corpdir/${nick}corp
+  mkznccorp $nick '#soul9' $zncbasedir $corpdir/${nick}corp
 done
